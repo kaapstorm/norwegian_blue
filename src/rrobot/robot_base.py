327773fe47407f3647797bfd7d0a208bf4188829
Norman Hooper <norman.hooper@voss-solutions.com>
2013-12-04 14:31:21 +0200

    Added coroutines to RobotBase

--- src/rrobot/robot_base.py
+++ src/rrobot/robot_base.py
@@ -1,3 +1,4 @@
+import asyncio
 from rrobot.game_client import GameClient
 
 
@@ -8,91 +9,106 @@ class RobotBase(object):
     RobotBase provides the following read-only properties:
      * coords: Cartesian coordinates of the robot with the origin in the
                southwest corner of the battlefield
-     * damage: A rating from 0 (healthy) to 99 (brink of destruction)
+     * damage: An integer percentage, where 0 is healthy and 100 is destroyed.
 
     Robot status can be read and set using:
      * heading: The robot's heading in radians clockwise from north
      * speed: The robot's speed in metres per second
 
-    The robot can attack another robot using the "strike" method.
+    The robot can attack another robot using the "attack" method. It takes no
+    parameters. It simply strikes out in front of the robot.
 
-    It is alerted to activity with the following methods:
-     * bumped: No data is passed. The robot will need to use the last radar
-               data and its coordinates to figure out what happened.
-     * was_struck: The robot was successfully attacked by another robot
-     * radar_updated: This method is called every 500 milliseconds with the
-                      latest radar data
+    The robot is notified of activity with the following coroutines:
+     * started: The game is started. Starting coordinates are sent.
+     * bumped: The (absolute) heading of the bump is sent.
+     * attacked: The robot was successfully attacked by another robot. The
+                 (absolute) heading of the attack is sent.
+     * radar_updated: This method is called at a regular interval with the
+                      latest radar data. The interval is configured in
+                      settings['radar_interval'].
 
     """
-    def __init__(self, id_):
-        self.__id = id_
-        self._game = GameClient()
-        self.__heading = self.__set_heading()
-        next(self.__heading)
-        self.__speed = self.__set_speed()
-        next(self.__speed)
+    # <METHODS_TO_OVERLOAD>
 
-    @property
-    def coords(self):
-        """Coordinates (origin southwest corner of battlefield)"""
-        return self._game.send_sync('get_coords', self.__id)
+    def started(self):
+        """
+        Called when the game starts
+        """
+        coords = yield
 
-    @property
-    def damage(self):
-        """Damage rating of 0 to 99"""
-        return self._game.send_sync('get_damage', self.__id)
-
-    @property
-    def heading(self):
-        """Heading (radians clockwise from north)"""
-        return self._game.send_sync('get_heading', self.__id)
-
-    @heading.setter
-    def heading(self, radians):
-        self.__heading.send(radians)
-
-    @property
-    def speed(self):
-        """Speed (metres per second)"""
-        return self._game.send_sync('get_speed', self.__id)
-
-    @speed.setter
-    def speed(self, mps):
-        self.__speed.send(mps)
-
-    def strike(self):
-        self._game.send_async('strike', self.__id)
+    def attacked(self):
+        """
+        Called when this robot is attacked by another robot
+        """
+        while True:
+            heading = yield
 
     def bumped(self):
         """
-        Called when robot bumps or is bumped
-        """
-        pass
-
-    def radar_updated(self, radar):
-        """
-        Called every 500ms
-        """
-        pass
-
-    def was_struck(self):
-        """
-        Called when robot is struck
-        """
-        pass
-
-    def __set_heading(self):
-        """
-        Coroutine that accepts a heading in radians clockwise from north
+        Called when robot this bumps or is bumped
         """
         while True:
-            radians = yield  # radians clockwise from north
-            self._game.send_async('set_heading', self.__id, radians)
+            heading = yield
 
-    def __set_speed(self):
+    def radar_updated(self):
         """
-        Coroutine that accepts a speed in metres per second
+        Called at a regular interval
         """
         while True:
-            mps = yield  # radians clockwise from north
-            self._game.send_async('set_speed', self.__id, mps)
+            radar = yield
+
+    # </METHODS_TO_OVERLOAD>
+
+    def __init__(self, id_):
+        self.__id = id_
+        self._client = GameClient()
+        # Start coroutines
+        next(self.started())
+        next(self.attacked())
+        next(self.bumped())
+        next(self.radar_updated())
+
+    @property
+    @asyncio.coroutine
+    def coords(self):
+        """Coordinates (origin southwest corner of battlefield)"""
+        #return self._game.send_sync('get_coords', self.__id)
+        coords = yield from self._client.send_async('get_coords', self.__id)
+        return coords
+
+    @property
+    @asyncio.coroutine
+    def damage(self):
+        """Damage rating of 0 to 99"""
+        #return self._game.send_sync('get_damage', self.__id)
+        damage = yield from self._client.send_async('get_damage', self.__id)
+        return damage
+
+    @property
+    @asyncio.coroutine
+    def heading(self):
+        """Heading (radians clockwise from north)"""
+        #return self._game.send_sync('get_heading', self.__id)
+        rads = yield from self._client.send_async('get_heading', self.__id)
+        return rads
+
+    @heading.setter
+    def heading(self, rads):
+        #self.__heading.send(rads)
+        self._client.send_async('set_heading', self.__id, rads)
+
+    @property
+    @asyncio.coroutine
+    def speed(self):
+        """Speed (metres per second)"""
+        #return self._game.send_sync('get_speed', self.__id)
+        mps = yield from self._client.send_async('get_speed', self.__id)
+        return mps
+
+    @speed.setter
+    def speed(self, mps):
+        #self.__speed.send(mps)
+        self._client.send_async('set_speed', self.__id, mps)
+
+    def attack(self):
+        self._client.send_async('attack', self.__id)
